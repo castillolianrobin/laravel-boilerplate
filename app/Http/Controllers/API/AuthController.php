d0cc2c1072e459343c7513849d5f4f289ee526be
Liana Bisuña <liana@thinkbitsolutions.com>
2023-08-17 14:06:41 +0800

    Chore: Add username in registration

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
-use App\Models\UserDetails;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -20,23 +19,16 @@ class AuthController extends Controller
             $user = User::create([
                 'first_name' => $request->input('first_name'),
                 'last_name' => $request->input('last_name'),
+                'username' => $request->input('username'),
                 'email' => $request->input('email'),
                 'password' => Hash::make($request->input('password'))
             ]);
-            
-            $userDetails = UserDetails::create([
-                'user_id' => $user->id,
-                'first_name' => $request->input('first_name'),
-                'last_name' => $request->input('last_name'),
-                'middle_name' => $request->input('middle_name'),
-                // 'profile_img_' => $request->input('middle_name'),
-            ]);
 
             // If user is not created
-            if (!$user || !$userDetails) {
+            if (!$user) {
                 return ApiResponse::noContent(config('constants.REGISTRATION_UNSUCCESSFUL'));
             }
-                          
+
             // Create token
             $token = $user->createToken('user_token')->plainTextToken;
 
@@ -49,14 +41,6 @@ class AuthController extends Controller
             );
         }
         catch (\Exception $e) {
-            // Delete any failed instances
-            $user = User::where('email', '=', $request->input('email'))->first();
-            if ($user) {
-                $user->delete();
-                UserDetails::where('user_id', '=', $user->id)
-                    ->delete();
-            }
-
             return ApiResponse::serverError($e->getMessage());
         }
     }
@@ -65,9 +49,7 @@ class AuthController extends Controller
     {
         try {
             // Find user
-            $user = User::with('userDetails')
-                ->where('email', '=', $request->input('email'))
-                ->first();
+            $user = User::where('email', '=', $request->input('email'))->first();
             
             // If user doesn't exist
             if (!$user) {
@@ -83,7 +65,7 @@ class AuthController extends Controller
                     config('constants.LOGIN_SUCCESSFUL'),
                     [
                         'user' => $user,
-                        'token' => $token,
+                        'token' => $token
                     ]
                 );
             }
