db138058577707fd0fde10f07f391c978766d26f
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-08-17 13:46:44 +0800

    Fix: ChatRoomMember pivot name

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -20,7 +20,15 @@ class ChatRoom extends Model
         return $this
             ->belongsToMany(User::class, 'chat_room_members')
             ->withPivot(['is_admin'])
-            ->as('chat_room_members');
+            ->as('chat_room_membership');
+    }
+
+    public function memberDetails(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(UserDetails::class, 'chat_room_members', 'chat_room_id', 'user_id', 'id', 'user_id')
+            ->withPivot(['is_admin'])
+            ->as('chat_room_membership');
     }
 
     public function messages()
